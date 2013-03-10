14ddfcab8f58ace5ac0cc340a9c0d9c1f18fdb95
Uriziel <urizieli@gmail.com>
2013-03-10 15:34:55 +0100

    Added setuptools

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='libmal',
+      version='0.1',
+      description='Library to manage MyAnimeList',
+      url='https://github.com/Uriziel/libMal',
+      author='Robert "Uriziel" Musil',
+      author_email='urizieli@gmail.com',
+      license='MIT',
+      packages=['libMal'],
+      zip_safe=False)
